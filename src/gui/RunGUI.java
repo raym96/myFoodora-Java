f45d208dfa128ed47d6239110e1660a24e3ff670
VincentHXA <136219065@qq.com>
2017-05-05 20:41:41 +0200

    Add files via upload

---
+++ src/gui/RunGUI.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.SwingUtilities;
+
+import gui.model.SystemData;
+
+public class RunGUI {
+	
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				SystemData.initialMyFoodora();
+				
+				Login login = new Login();
+			}
+		});
+		
+	}
+}
